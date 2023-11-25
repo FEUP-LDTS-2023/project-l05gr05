159fa560abdb8b1e3ff76b0ed0f534502b7b765d
bialachado <blachadop@gmail.com>
2023-11-25 22:01:03 +0000

    Added AppTest and GameTest

---
+++ src/test/java/com/aor/pacxon/AppTest.java
@@ -0,0 +1,21 @@
+package com.aor.pacxon;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class AppTest {
+    /*
+    @Test
+    public void testAppInitialization() {
+        //testa a inicialização e o início da execução do jogo na classe Game
+        Game game = new Game();
+        assertNotNull(game);
+        game.start();
+        assertNotNull(game.getCurrentLevel()); // getCurrentLevel() por implementar
+        assertTrue(game.isRunning()); // isRunning() por implementar
+
+    }
+     */
+}
