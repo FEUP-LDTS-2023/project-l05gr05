810c9e9562d7eeceee1307b0e2bdb3321c67d49b
franciscasf <franciscasf299@gmail.com>
2023-11-26 17:11:21 +0000

    Added all tests for model

---
+++ src/test/java/com/aor/pacxon/model/MenuWinLevelTest.java
@@ -0,0 +1,71 @@
+package com.aor.pacxon.model;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class MenuWinLevelTest {
+    //Verifica se a criação de um objeto MenuWinLevel atribui corretamente os pontos e se o número de entradas e a primeira entrada selecionada estão corretos por padrão
+    @Test
+    void testInitialization() {
+        MenuWinLevel menu = new MenuWinLevel(100);
+        assertEquals(100, menu.getPoints());
+        assertEquals(2, menu.getNumberEntries());
+        assertTrue(menu.isSelectedNextLevel());
+        assertFalse(menu.isSelectedBackToMenu());
+    }
+
+    //Verifica se a chamada ao método nextEntry avança corretamente para a próxima entrada, alterando o estado de seleção conforme esperado
+    @Test
+    void testNextEntry() {
+        MenuWinLevel menu = new MenuWinLevel(100);
+        menu.nextEntry();
+        assertFalse(menu.isSelectedNextLevel());
+        assertTrue(menu.isSelectedBackToMenu());
+    }
+
+
+    //Verifica se a chamada ao método previousEntry retrocede corretamente para a entrada anterior, alterando o estado de seleção conforme esperado
+    @Test
+    void testPreviousEntry() {
+        MenuWinLevel menu = new MenuWinLevel(100);
+        menu.previousEntry();
+        assertFalse(menu.isSelectedNextLevel());
+        assertTrue(menu.isSelectedBackToMenu());
+    }
+
+    //ERRO
+    //Verifica se a seleção de entradas cicla corretamente, indo para a próxima e, em seguida, para a anterior, e mantendo o estado de seleção esperado
+    /*@Test
+    void testCycleEntries() {
+        MenuWinLevel menu = new MenuWinLevel(100);
+        menu.nextEntry();
+        assertTrue(menu.isSelectedNextLevel());
+        assertFalse(menu.isSelectedBackToMenu());
+
+        menu.previousEntry();
+        assertFalse(menu.isSelectedNextLevel());
+        assertTrue(menu.isSelectedBackToMenu());
+    }
+    */
+
+    //Verifica se o método getEntry retorna corretamente as strings das entradas esperadas
+    @Test
+    void testGetEntry() {
+        MenuWinLevel menu = new MenuWinLevel(100);
+        assertEquals("NEXT LEVEL", menu.getEntry(0));
+        assertEquals("BACK TO MENU", menu.getEntry(1));
+    }
+
+
+    //Verifica se o método isSelected retorna verdadeiro para a entrada corretamente selecionada e falso para a não selecionada
+    @Test
+    void testIsSelected() {
+        MenuWinLevel menu = new MenuWinLevel(100);
+        assertTrue(menu.isSelectedNextLevel());
+        assertFalse(menu.isSelectedBackToMenu());
+
+        menu.nextEntry();
+        assertFalse(menu.isSelectedNextLevel());
+        assertTrue(menu.isSelectedBackToMenu());
+    }
+
+}
---
+++ src/test/java/com/aor/pacxon/model/WallTest.java
@@ -0,0 +1,28 @@
+package com.aor.pacxon.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WallTest {
+
+    //Testa se a wall é inicializada com a posição correta
+    @Test
+    void testInitialization() {
+        Wall wall = new Wall(2, 3);
+        assertEquals(new Position(2, 3), wall.getPosition());
+    }
+
+    //Testa se a posição da wall é alterada
+    @Test
+    void testPositionChange() {
+        Wall wall = new Wall(2, 3);
+        wall.setPosition(new Position(4, 5));
+        assertEquals(new Position(4, 5), wall.getPosition());
+    }
+
+    //por implementar: testa interações entre as wall e outros objetos
+    @Test
+    void testWallInteractions() {
+    }
+}
